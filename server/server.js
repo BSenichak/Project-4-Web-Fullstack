0a0f90c31b842df33a262f290ea0d7502f3754b6
BSenichak <bsenk77@gmail.com>
2025-01-15 13:21:20 +0200

    add posters to db

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("static"));
 
 function getMovie(req, res) {
     const videoPath = path.resolve(__dirname, "movies", req.filename);
