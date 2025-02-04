46e8e8f7cf09ec665c0ef334cf206242ea3f35aa
BSenichak <bsenk77@gmail.com>
2025-02-04 13:04:45 +0200

    add internationalization support with translations for English, Ukrainian, French, and German; update database connection to use environment variables; add .env to .gitignore

---
+++ client/src/server.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: false,
+});
+
+instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status >= 400) {
+            throw new Error(error.response.data);
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+const env = require('dotenv').config().parsed;
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'movies_db',
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+    port: env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
