01a5b8ec9bbb92b4779e824584a27b4561d7c46d
BSenichak <bsenk77@gmail.com>
2025-02-04 13:04:45 +0200

    add internationalization support with translations for English, Ukrainian, French, and German; update database connection to use environment variables; add .env to .gitignore

--- server/sql.sql
+++ server/sql.sql
@@ -1,10 +1,13 @@
-CREATE DATABASE movies_db;
-CREATE TABLE movies(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    release_data DATE NOT NULL,
-    rating DECIMAL(2, 1) NOT NULL,
-    duration INT NOT NULL,
-    description TEXT NOT NULL,
-    filename VARCHAR(255) NOT NULL
-);
+-- Active: 1738665443110@@sql.freedb.tech@3306@freedb_dateloger
+CREATE TABLE `movies` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `release_data` date NOT NULL,
+    `rating` decimal(2, 1) NOT NULL,
+    `duration` int NOT NULL,
+    `description` text NOT NULL,
+    `filename` varchar(255) NOT NULL,
+    `poster_url` varchar(255) DEFAULT NULL,
+    `likes` int DEFAULT '0',
+    PRIMARY KEY (`id`)
+)
